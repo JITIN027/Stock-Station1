56ecd483f1fcfadb078c1a6f849cdc985e33f691
Jitin <jitin.a27.jd@gmail.com>
2022-01-03 17:51:17 +0530

    db update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb+srv://admin:karthikhegd6960@cluster0.slipv.mongodb.net/registered?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://jitin:jitin027@cluster0.slipv.mongodb.net/registered?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    
+
 }).then(()=>{
     console.log(`conn is successful`);
 }).catch((err)=>{
--- src/models/register.js
+++ src/models/register.js
@@ -1,6 +1,24 @@
 const mongoose=require('mongoose');
 const bcrypt=require('bcryptjs');
 
+const transactionschema=new mongoose.Schema({
+  transaction_number:{
+    type:Number
+  },
+  stock_name:{
+    type:String
+  },
+  quantity:{
+    type:Number
+  },
+  buying_price:{
+    type:Number
+  },
+  total_price:{
+    type:Number
+  }
+});
+
 const registerSchema=new mongoose.Schema({
     firstname:{
         type:String,
@@ -22,7 +40,7 @@ const registerSchema=new mongoose.Schema({
     phoneno:{
         type:Number,
         required:true,
-        
+
     },
     password:{
         type:String,
@@ -31,7 +49,8 @@ const registerSchema=new mongoose.Schema({
     confirmpassword:{
         type:String,
         required:true
-    }
+    },
+    transactions:[transactionschema]
 })
 registerSchema.pre("save",async function(next){
     this.password=bcrypt.hash(this.password,10);
